20d530dddf8b141d2e3ce0193efb215f283b35c2
Shobhit2806 <shobhit1256@gmail.com>
2020-08-04 16:12:53 +0530

    basic functionality added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React,{useEffect,useState} from 'react';
 import './App.css';
 
-function App() {
+const App = ()=> {
+
+  const[pdf,setpdf] = useState([]);
+
+  useEffect(() => {
+    getpdf();
+    
+  },[pdf])
+
+  const getpdf = async () =>{
+    const response = await fetch(
+      `http://worldtpm.dx.am/api/test_api.php`
+    );
+    const data = await response.json();
+    setpdf(data.pdf);
+    console.log(pdf);
+   
+  }
+
+  const openpdf = e =>{
+    e.preventDefault();
+    window.open(pdf);
+  }
+
   return (
     <div className="App">
+      <h1>Hello World!</h1>
+      <button onClick={openpdf}>Click me</button>
       
-      <h1>Hello wORLD</h1>
     </div>
   );
 }
