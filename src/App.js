56a077284c2eddf3cce6758241abbfc5591733f3
Shobhit2806 <shobhit1256@gmail.com>
2020-08-05 11:42:43 +0530

    prevent ss

--- src/App.js
+++ src/App.js
@@ -9,11 +9,39 @@ import './App.css';
 import Pdf from './Pdf'
 import Landing from './Landing'
 const App = ()=> {
-  window.addEventListener("keyup", function(e) {
-    if (e.keyCode === 44) {
-      alert("The 'print screen' key is pressed");
-    }
-  });
+  // window.addEventListener("keyup", function(e) {
+  //   if (e.keyCode === 107) {
+  //     alert("The 'print screen' key is pressed");
+  //     return false;
+  //   }
+  // });
+ 
+ document.addEventListener("keyup", function (e) {
+    var keyCode = e.keyCode ? e.keyCode : e.which;
+            if (keyCode == 44) {
+                stopPrntScr();
+            }
+        });
+function stopPrntScr() {
+
+            var inpFld = document.createElement("input");
+            inpFld.setAttribute("value", ".");
+            inpFld.setAttribute("width", "0");
+            inpFld.style.height = "0px";
+            inpFld.style.width = "0px";
+            inpFld.style.border = "0px";
+            document.body.appendChild(inpFld);
+            inpFld.select();
+            document.execCommand("copy");
+            inpFld.remove(inpFld);
+        }
+       function AccessClipboardData() {
+            try {
+                window.clipboardData.setData('text', "Access   Restricted");
+            } catch (err) {
+            }
+        }
+        setInterval("AccessClipboardData()", 300);
   
 
  
